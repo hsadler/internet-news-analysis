0b84979e57833331d21573c814a4cc469643e1a3
Harry Sadler <harrysadlermusic@gmail.com>
2016-11-20 18:37:47 -0800

    init commit

---
+++ scripts/scrape-scripts/google-news-rss-scrape.py
@@ -0,0 +1,4 @@
+
+scrape_url = 'https://news.google.com/news?output=rss'
+
+print('scraping {0}...'.format(scrape_url))
---
+++ scripts/scrape-scripts/newsapi-json-scrape.py
@@ -0,0 +1,8 @@
+
+news_api = {
+    get_sources_url: 'https://newsapi.org/v1/sources?language=en',
+    get_articles_url: 'https://newsapi.org/v1/articles',
+    api_key: '9110ce6d759d455392a0d83606bf59db'
+}
+
+print('scraping...')
