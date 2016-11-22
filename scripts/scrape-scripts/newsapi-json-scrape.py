de142d7a58c92eb1ab2f6042dc5c223dc81e2925
Harry Sadler <harrysadlermusic@gmail.com>
2016-11-21 22:26:41 -0800

    update TODO, wip newsapi scrape script

--- scripts/scrape-scripts/newsapi-json-scrape.py
+++ scripts/scrape-scripts/newsapi-json-scrape.py
@@ -1,8 +1,30 @@
 
+import requests
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
 news_api = {
-    get_sources_url: 'https://newsapi.org/v1/sources?language=en',
-    get_articles_url: 'https://newsapi.org/v1/articles',
-    api_key: '9110ce6d759d455392a0d83606bf59db'
+    'get_sources_url': 'https://newsapi.org/v1/sources?language=en',
+    'get_articles_url': 'https://newsapi.org/v1/articles',
+    'api_key': '9110ce6d759d455392a0d83606bf59db'
 }
 
-print('scraping...')
+sources_res = requests.get(news_api['get_sources_url'])
+sources_data = sources_res.json()['sources']
+
+
+source_ids = []
+
+for sources_item in sources_data:
+    source_id = sources_item[u'id']
+    source_ids.append(source_id)
+
+
+for s_id in source_ids:
+    print('making source id api calls...')
+
+
+
