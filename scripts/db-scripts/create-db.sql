bdbff2e9f49abf45920d3d8b6f625344a61c7301
Harry Sadler <harrysadlermusic@gmail.com>
2016-11-23 11:10:43 -0800

    write notes, add create-db.sql script

---
+++ scripts/db-scripts/create-db.sql
@@ -0,0 +1,4 @@
+
+# create project database
+
+CREATE DATABASE internet_news_analysis;
--- scripts/scrape-scripts/google-news-rss-scrape.py
+++ scripts/scrape-scripts/google-news-rss-scrape.py
@@ -1,4 +1,12 @@
 
+import requests
+import pprint
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
 scrape_url = 'https://news.google.com/news?output=rss'
+
 
 print('scraping {0}...'.format(scrape_url))
