abced668f0f05d9be032f2199c1893d2ccc63ae9
Harry Sadler <harrypatricksadler@gmail.com>
2016-11-26 19:28:12 -0800

    work on google-rss scrape script

--- scripts/scrape/google-news-rss-scrape.py
+++ scripts/scrape/google-news-rss-scrape.py
@@ -2,11 +2,22 @@
 import requests
 import pprint
 
+from user_agent import get_rand_user_agent as rand_user_agent
+from xml.etree import ElementTree
+
 
 pp = pprint.PrettyPrinter(indent=4)
 
 
 scrape_url = 'https://news.google.com/news?output=rss'
 
+scrape_headers = {
+    'User-Agent': rand_user_agent()
+}
 
-print('scraping {0}...'.format(scrape_url))
+res = requests.get(scrape_url, headers=scrape_headers)
+tree = ElementTree.fromstring(res.content)
+
+items = tree.find('channel').findall('item')
+
+pp.pprint(items)
---
+++ scripts/scrape/store_news.py
@@ -0,0 +1,3 @@
+
+def store_news_item():
+	#
