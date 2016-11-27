d1d83eb1b2806e069a26ac9b84816776c2576f8c
Harry Sadler <harrysadlermusic@gmail.com>
2016-11-26 20:28:38 -0800

    add create-tables script

--- scripts/db/create-tables.py
+++ scripts/db/create-tables.py
@@ -1,3 +1,20 @@
 
-# create tables script
+import MySQLdb as mdb
+
+
+con = mdb.connect('localhost', 'root', '', 'internet_news_analysis')
+cur = con.cursor()
+
+with con:
+
+    # create news_items table
+    cur.execute("""CREATE TABLE IF NOT EXISTS news_items(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        url VARCHAR(255),
+        author VARCHAR(100),
+        title VARCHAR(255),
+        description VARCHAR(255),
+        scrape_ts INT(12),
+        publish_ts INT(12)
+    );""")
 
