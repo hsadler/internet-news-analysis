be236af91ea01f9aa782aaa4184f7605f8247653
Harry Sadler <harrysadlermusic@gmail.com>
2016-12-06 08:57:18 -0800

    add project notes

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from scripts.scrape.google_news_scrape import scrape as g_scrape
 from scripts.scrape.newsapi_scrape import scrape as n_scrape
 
 
+# scrapers
 def google_news_scrape():
     g_scrape()
 
 def newsapi_scrape():
     n_scrape()
+
+
