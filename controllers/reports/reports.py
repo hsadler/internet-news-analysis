c16eb1a8e9322eaf452d730183da8e8c4235b8bc
Harry Sadler <harrysadlermusic@gmail.com>
2016-12-15 08:15:10 -0800

    wip Reports class

---
+++ controllers/reports/reports.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+# Reports Controller
+
+import pprint
+import logging
+
+from models.article.article import Article
+from models.headline_word.headline_word import HeadlineWord
+
+pp = pprint.PrettyPrinter(indent=4)
+logging.basicConfig(filename='logs/scrape.log', level=logging.DEBUG)
+
+
+
+class Reports():
+
+
+
+    def method():
+        pass
+
+
+
+
